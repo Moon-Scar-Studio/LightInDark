9f143d3628bbb7a459f7a2f89ac2b2d7e84fbbfb
hvtXsvc <hvtXsvc@outlook.com>
2026-09-24 22:58:41 +0800

    feat：加了两条注释

--- Light/Patches/DisableLobbyEnginePowerPatch.cs
+++ Light/Patches/DisableLobbyEnginePowerPatch.cs
@@ -11,12 +11,12 @@ public static class DisableLobbyEnginePowerPatch
         {
             if (Input.GetKeyDown(KeyCode.LeftControl))
             {
-                var LeftEngine = __instance.transform.GetChild(2);
+                var LeftEngine = __instance.transform.GetChild(2); // 左引擎火
                 LeftEngine.gameObject.SetActive(!LeftEngine.gameObject.activeSelf);
             }
             if(Input.GetKeyDown(KeyCode.RightControl))
             {
-                var RightEngine = __instance.transform.GetChild(1);
+                var RightEngine = __instance.transform.GetChild(1); // 右引擎火
                 RightEngine.gameObject.SetActive(!RightEngine.gameObject.activeSelf);
             }
         }
